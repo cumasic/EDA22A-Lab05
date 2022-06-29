7c6c8cfe90f435861be603d16ee68f941f29d7af
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-06-28 21:40:37 -0500

    Se creó el main donde recibe la cadena y la verifica

--- CorchetesEquilibrados/CorchetesEquilibrados.java
+++ CorchetesEquilibrados/CorchetesEquilibrados.java
@@ -1,10 +1,19 @@
 package CorchetesEquilibrados;
-
+import java.util.*;
 public class CorchetesEquilibrados {
     public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+		System.out.println("Ingrese una cadena:");
+		String cadena=sc.next();
+		boolean cond = isBalanced(cadena);
+		if(cond)
+            System.out.println("La cadena está equilibrada");
+        else
+            System.out.println("La cadena no está equilibrada");
 
     }
     public static boolean isBalanced(String a){
+        
         return true;
     }
 }
