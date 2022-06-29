8ea0ba68ed49b1c931f6187d85939ae338106769
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-06-28 21:37:48 -0500

    Se creó la función isBalanced para balancear los corchetes

---
+++ CorchetesEquilibrados/CorchetesEquilibrados.java
@@ -0,0 +1,10 @@
+package CorchetesEquilibrados;
+
+public class CorchetesEquilibrados {
+    public static void main(String[]args){
+
+    }
+    public static boolean isBalanced(String a){
+        return true;
+    }
+}
