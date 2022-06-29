1442c43fc8fb2866a3b5e0d0b2118920a32fd56e
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-06-28 22:21:21 -0500

    Se creó una clase Test donde se probará el ArbolAVL

---
+++ ArbolAVL/Test.java
@@ -0,0 +1,24 @@
+package ArbolAVL;
+
+public class Test {
+    public static void main(String[]args) {
+        AVL<Integer> b = new AVL<Integer>();
+        try {
+            Integer[] data = {100,200,300,400,50,25,350,365,120};
+            for (Integer i : data) {
+                b.insert(i);
+            }
+            System.out.println(b);
+            b.remove(100);
+            System.out.println(b);
+            b.search(100);
+            System.out.println(b);
+            b.search(200);
+            System.out.println(b);
+            
+        }
+        catch(Exception x){
+            System.out.println(x.getMessage());
+        }
+     }
+}
